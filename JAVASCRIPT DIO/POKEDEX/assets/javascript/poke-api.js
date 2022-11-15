696c1bf33614044ee4d3952c38ea49ab954044c6
Chronald <cachristian912@gmail.com>
2022-11-14 21:23:55 -0300

    Adicionando botão para passar para proxima pagina

--- JAVASCRIPT DIO/POKEDEX/assets/javascript/poke-api.js
+++ JAVASCRIPT DIO/POKEDEX/assets/javascript/poke-api.js
@@ -25,7 +25,7 @@ pokeApi.getPokemonsDetail = (pokemon) => {
     .then(convertPokeApiDetailToPokemon) 
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 50) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     
     return fetch(url)
